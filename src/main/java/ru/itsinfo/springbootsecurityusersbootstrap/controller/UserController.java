b2a5bad81d7086610c88942a05fed6251bbf20b6
Evgeny Voinov <joekobarnd@gmail.com>
2023-02-02 18:28:42 +0300

    13

--- src/main/java/ru/itsinfo/springbootsecurityusersbootstrap/controller/UserController.java
+++ src/main/java/ru/itsinfo/springbootsecurityusersbootstrap/controller/UserController.java
@@ -1,20 +1,40 @@
 package ru.itsinfo.springbootsecurityusersbootstrap.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import ru.itsinfo.springbootsecurityusersbootstrap.model.User;
+import ru.itsinfo.springbootsecurityusersbootstrap.service.UserService;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String welcomePage(Model model, HttpSession session,
+                              @SessionAttribute(required = false, name = "Authentication-Name") String authenticationName) {
+        userService.authenticateOrLogout(model, session, authenticationName);
+        return "index";
+    }
+
     @GetMapping("")
     public String showUserInfo(@CurrentSecurityContext(expression = "authentication.principal") User principal,
                                Model model) {
         model.addAttribute("user", principal);
-        return "fragments/user-info";
+        return "fragments/userPage";
     }
+
 }
